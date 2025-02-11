a004491d4a075a3ad426f4f60409691ef568d30a
princesinghrajput <psr8084@gmail.com>
2025-02-11 22:46:05 +0530

    feat: implement login api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
-const registerUser = async(req, res, next)=>{
+const registerUser = async(req, res)=>{
     try {
         const {username, email, password} = req.body;
         console.log(username, email, password);
@@ -35,6 +35,44 @@ const registerUser = async(req, res, next)=>{
     }
 }
 
+const loginUser = async(req, res)=>{
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(401).json({message: "Invalid email or password"});
+        }
+
+        //check password
+        const isMatch = await user.comparePassword(password);
+
+        if(!isMatch){
+            return res.status(401).json({message: "Invalid credentials"});
+        }
+
+        const token = jwt.sign({
+            userId: user._id,
+            username: user.username,
+        }, process.env.JWT_SECRET, {expiresIn: "1h"});
+
+
+        res.status(200).json({
+            token,
+            user:{
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            }
+        })
+        
+    } catch (error) {
+        res.status(500).json({message: "Error logging in", error: error.message});
+        
+    }
+}
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
