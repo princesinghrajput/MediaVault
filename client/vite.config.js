b6b698f7c2e23df32d084a8396dd575ef3cd59b4
princesinghrajput <psr8084@gmail.com>
2025-02-14 00:14:37 +0530

    fix: api url

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:6000',
+        target: 'https://mediavault-xx3y.onrender.com',
         changeOrigin: true,
       }
     }
