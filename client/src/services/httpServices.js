087b94f8313fd8baf378095b092fd453bacc0252
princesinghrajput <prince.kumar@sensationsolutions.in>
2025-02-14 09:58:40 +0530

    chore: remove proxy to prevent error

--- client/src/services/httpServices.js
+++ client/src/services/httpServices.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://mediavault-xx3y.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
